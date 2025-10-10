2a08be67824fe22a5c81731d9c1e8ee3a5f74f6c
Rani Jaselia <ranijaselia241@gmail.com>
2025-10-10 19:29:22 +0700

    Add files via upload

---
+++ lat4.3.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    const int MAX = 10;   // kapasitas maksimum stack
+    int stack[MAX];       
+    int top = -1;         
+    int data;             
+
+    cout << "=== PUSH Data ke Stack ===\n";
+
+    while (true) {
+        if (top == MAX - 1) {  
+            cout << "Stack Penuh! Tidak bisa menambah data lagi.\n";
+            break;
+        }
+
+        cout << "Masukkan data ke stack: ";
+        cin >> data;
+
+        top++;
+        stack[top] = data;
+        cout << "Data " << data << " berhasil di-PUSH ke stack.\n";
+    }
+
+    return 0;
+}
+
