46b6fa7ddb8d21c328b308a18ffa5e835c48fe99
Rani Jaselia <ranijaselia241@gmail.com>
2025-10-23 10:38:38 +0700

    Delete lat2.1.cpp

--- lat2.1.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main(){
-	unsigned int base = 0x1000;
-	unsigned int alamat = base + 8 * sizeof(char);
-	cout<<hex<<"Alamat &x[8] = "<<alamat<<"H"<<endl;
-	return 0;
-}
